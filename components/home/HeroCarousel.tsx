0d367a35cba5eab5970f555898be6875256c5a3c
sathedhiraj <dhirajsathe2024@gmail.com>
2026-08-07 18:15:57 +0530

    new update

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -9,7 +9,7 @@ const SLIDES = [
     id: 1,
     image: "/hero-banner.png",
     tagline: "Real Routines",
-    heading: "Shardaha sathe Body Care\nBacked by Science",
+    heading: "Kapil tavde Body Care\nBacked by Science",
     sub: "Formulated with science-backed actives for skin that actually transforms.",
     cta: "Shop the Collection",
     href: "/collections/shop",
