d7cabb549a7d0da34924d3e3434f4f4a6e5605e7
sathedhiraj <156566905+sathedhiraj@users.noreply.github.com>
2026-08-11 11:42:11 +0530

    Update heading in HeroCarousel component

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -9,7 +9,7 @@ const SLIDES = [
     id: 1,
     image: "/hero-banner.png",
     tagline: "Real Routines",
-    heading: "jatin sathe Body Care\nBacked by Science",
+    heading: "0000000000 Care\nBacked by Science",
     sub: "Formulated with science-backed actives for skin that actually transforms.",
     cta: "Shop the Collection",
     href: "/collections/shop",
